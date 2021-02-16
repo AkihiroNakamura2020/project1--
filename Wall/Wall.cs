aa8358d8b60a643177483995901f558901105602
AkihiroNakamura2020 <72118975+AkihiroNakamura2020@users.noreply.github.com>
2021-02-16 21:47:04 +0900

    wall

---
+++ Wall/Wall.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Wall : MonoBehaviour
+{
+    private GameObject player;   //プレイヤー情報格納用
+
+    Vector3 pos;
+    Vector3 playerpos;
+
+    private GameObject mainCamera;      //メインカメラ格納用
+    private GameObject subCamera;       //サブカメラ格納用 
+
+
+    // Use this for initialization
+    void Start()
+    {
+        this.player = GameObject.Find("Player");
+        mainCamera = GameObject.Find("MainCamera");
+        subCamera = GameObject.Find("SubCamera");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos = transform.position;
+        playerpos = this.player.transform.position;
+
+        if (pos == playerpos)
+        {
+            subCamera.transform.SetParent(null);
+            Destroy(player);
+
+            Invoke("Over", 0.0f);
+
+        }
+
+    }
+    
+    void Over()
+    {
+        
+        SceneManager.LoadScene("GameOver");
+    }
+}
---
+++ Wall/Wall2.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall2 : MonoBehaviour
+{
+    private GameObject player;   //プレイヤー情報格納用
+
+    Vector3 pos;
+    Vector3 playerpos;
+    
+
+    private GameObject mainCamera;      //メインカメラ格納用
+    private GameObject subCamera;       //サブカメラ格納用
+
+    Ray ray;
+    RaycastHit hit;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        this.player = GameObject.Find("Wall");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        pos = transform.position;
+        playerpos = this.player.transform.position;
+        //Vector3 startpos = new Vector3(0, 0, 0);
+
+        if (pos == playerpos)
+        {
+            playerpos = new Vector3(0, 0, 0); 
+            
+        }
+
+    }
+
+}
+
+
