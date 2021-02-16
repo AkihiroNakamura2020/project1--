96b98a389a92b79555aad193305bd3cdac653d41
AkihiroNakamura2020 <72118975+AkihiroNakamura2020@users.noreply.github.com>
2021-02-16 21:43:23 +0900

    Add files via upload

---
+++ stagecreate/TrickGoal.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrickGoal : MonoBehaviour
+{
+
+    [SerializeField] GameObject test;
+    [SerializeField] GameObject test1;
+
+    int count = 0;
+
+
+    void Update()
+    {
+
+        if (Input.anyKeyDown)
+        {
+            count++;
+        }
+
+
+        if (count == 0)
+        {
+            //ゲームオブジェクト表示→非表示
+            test.SetActive(true);
+            test1.SetActive(false);
+        }
+
+        if (count == 17)
+        {
+            //ゲームオブジェクト表示→非表示
+            test.SetActive(false);
+            test1.SetActive(true);
+        }
+
+    }
+}
